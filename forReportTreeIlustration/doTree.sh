b9a7d77edd885c2f6719fc4d5ca3a258c4fc377b
clemaero <clem.thibault@sfr.fr>
2025-05-12 09:32:22 +0200

    .

--- forReportTreeIlustration/doTree.sh
+++ forReportTreeIlustration/doTree.sh
@@ -1,10 +1,14 @@
-mkdir dirTest
+rm -rf '<case>'
 mkdir '<case>'
-mkdir 0
 cd '<case>'
     mkdir 0  0.orig  '<time directories>'  constant  system  tests
-cd ../system
-    mkdir blockMeshDict controlDict decomposeParDict fvSchemes fvSolution
-cd ../0
-    mkdir k  omega  p  U
-cp -r 0 0.orig
+    cd system
+        mkdir blockMeshDict controlDict decomposeParDict fvSchemes fvSolution
+    cd ../0
+        mkdir k  omega  p  U
+    cd ..
+    cp -r 0 0.orig
+cd ..
+tree -L 3
+gnome-screenshot
+xwd -out arbo.xwd
